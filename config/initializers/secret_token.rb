ca448bff370f6fbc849cdbeb487f46caf8896bb1
Elia Schito <elia@schito.me>
2012-11-15 19:11:51 +0100

    Vanilla `rails new ruby_day -T`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyDay::Application.config.secret_token = '8d04d0448e27bc7c0be163b2090ba39533f61eb1fb62a47c63a2618172806cf797f10c9a01632da3a1fd0b092ba095478d782d5b8648c56c7facb5d565c97dc8'
